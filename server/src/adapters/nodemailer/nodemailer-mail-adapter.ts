45870cc0fc5debca3f795f556fa1ceb1f1dac95f
Claudia Batista <claudia@Beatrizs-Mac-mini.local>
2022-05-07 16:32:38 +0200

    implement backend

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "fc98a469c89497",
+    pass: "46321f23931873",
+  },
+});
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendEmail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi.feedget.com>",
+      to: "Claudia Batista <contatodaclau@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
