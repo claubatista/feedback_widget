d3c9eef3143b908d7cc16f866b5888dc482b90dc
Claudia Batista <claudia@Beatrizs-Mac-mini.local>
2022-05-07 16:15:18 +0200

    add the WidgetForm component

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,68 @@
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Bug",
+    image: {
+      source: bugImageUrl,
+      alt: "bug image",
+    },
+  },
+  IDEA: {
+    title: "Idea",
+    image: {
+      source: ideaImageUrl,
+      alt: "lamp image",
+    },
+  },
+  Other: {
+    title: "Other",
+    image: {
+      source: thoughtImageUrl,
+      alt: "",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanges={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela <a href="https://rocketseat.com.br">Rocketseat</a>{" "}
+      </footer>
+    </div>
+  );
+}
